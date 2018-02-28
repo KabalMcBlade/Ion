5e6b345458a7581a591625433000f0e4437caec3
KabalMcBlade <michele.condo@hotmail.com>
2018-02-28 12:44:03 +0000

    Change RenderManager into RenderContext, not singleton anymore, Minor Fix

---
+++ Ion/Renderer/RenderContext.h
@@ -0,0 +1,63 @@
+#pragma once
+
+
+#define VK_USE_PLATFORM_WIN32_KHR
+#include <vulkan/vulkan.h>
+
+#include "../Dependencies/Eos/Eos/Eos.h"
+#include "../Dependencies/vkMemoryAllocator/vkMemoryAllocator/vkMemoryAllocator.h"
+
+#include "../Core/CoreDefs.h"
+
+#include "GPU.h"
+
+EOS_USING_NAMESPACE
+VK_ALLOCATOR_USING_NAMESPACE
+
+ION_NAMESPACE_BEGIN
+
+class ION_DLL RenderContext
+{
+public:
+    ionBool    Init(HINSTANCE _instance, HWND _handle, ionBool _enableValidationLayer);
+    void       Shutdown();
+
+    RenderContext();
+    ~RenderContext();
+
+private:
+    RenderContext(const RenderContext& _Orig) = delete;
+    RenderContext& operator = (const RenderContext&) = delete;
+
+private:
+    // They are in order to call
+    ionBool CreateInstance(ionBool _enableValidationLayer);
+    ionBool CreatePresentationSurface(HINSTANCE _instance, HWND _handle);
+    ionBool CreatePhysicalDevice();
+    ionBool CreateLogicalDeviceAndQueues();
+    ionBool CreateSemaphores();
+    ionBool CreateQueryPool();
+    ionBool CreateCommandPool();
+    ionBool CreateCommandBuffer();
+    ionBool CreateSwapChain();
+    void    DestroySwapChain();
+    ionBool CreateRenderTargets();
+    void    DestroyRenderTargets();
+    ionBool CreateRenderPass();
+    ionBool CreateFrameBuffers();
+    void    DestroyFrameBuffers();
+
+private:
+    GPU                 m_vkGPU;                  //  access through this component to get value such m_vkPhysicalDevice
+    VkDevice            m_vkDevice;
+    VkSurfaceKHR        m_vkSurface;
+    VkInstance          m_vkInstance;
+    VkQueue			    m_vkGraphicsQueue;
+    VkQueue			    m_vkPresentQueue;
+    ionS32			    m_vkGraphicsFamilyIndex;
+    ionS32			    m_vkPresentFamilyIndex;
+
+    ionBool             m_vkValidationEnabled;
+};
+
+ION_NAMESPACE_END
