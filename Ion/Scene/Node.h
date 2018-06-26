4f620ffe754c82c1698948dbb3b51740029b64cf
KabalMcBlade <michele.condo@hotmail.com>
2018-06-26 14:24:36 +0100

    Added render layer for nodes

--- Ion/Scene/Node.h
+++ Ion/Scene/Node.h
@@ -21,6 +21,24 @@ enum ENodeType
     ENodeType_Camera
 };
 
+
+// this is used to group the entities under the same render camera
+enum ENodeRenderLayer : ionU32
+{
+    ENodeRenderLayer_Default = 0b00000000000000000000000000000000,
+    ENodeRenderLayer_1 = 0b00000000000000000000000000000001,
+    ENodeRenderLayer_2 = 0b00000000000000000000000000000010,
+    ENodeRenderLayer_3 = 0b00000000000000000000000000000100,
+    ENodeRenderLayer_4 = 0b00000000000000000000000000001000,
+    ENodeRenderLayer_5 = 0b00000000000000000000000000010000,
+    ENodeRenderLayer_6 = 0b00000000000000000000000000100000,
+    ENodeRenderLayer_7 = 0b00000000000000000000000001000000,
+
+    // for now 8 camera are enough.
+    // if need more, add here!
+};
+
+
 class Node;
 typedef SmartPointer<Node> NodeHandle;
 
@@ -52,6 +70,12 @@ public:
     void AttachToParent(NodeHandle& _parent);
     void DetachFromParent();
 
+    void AddToRenderLayer(ENodeRenderLayer _layer) { m_renderLayer |= _layer; }
+    void RemoveFromRenderLayer(ENodeRenderLayer _layer) { m_renderLayer &= ~_layer; }
+    ionU32 GetRenderLayer() { return m_renderLayer; }
+    ionBool IsInRenderLayer(ionU32 _layers) { return (m_renderLayer & _layers) != 0; }
+    ionBool IsInRenderLayer(ENodeRenderLayer _layer) { return (m_renderLayer & static_cast<ionU32>(_layer)) != 0; }
+
     //////////////////////////////////////////////////////////////////////////
 
     ENodeType GetNodeType() const { return m_nodeType; }
@@ -79,6 +103,7 @@ private:
     Node(const Node& _Orig) = delete;
     Node& operator = (const Node&) = delete;
 
+    ionU32 m_renderLayer;
     ionU32 m_nodeIndex;
     ionSize m_hash;
 
